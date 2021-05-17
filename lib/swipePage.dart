b0d4844a9b45481f2d860a59a777c862f6939716
GlendonK <36408879+GlendonK@users.noreply.github.com>
2021-05-17 21:32:56 +0800

    swipe page

--- lib/swipePage.dart
+++ lib/swipePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:swipe_cards/swipe_cards.dart';
 
 class SwipePage extends StatefulWidget {
   @override
@@ -8,18 +9,126 @@ class SwipePage extends StatefulWidget {
 }
 
 class SwipePageState extends State<SwipePage> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-          home: Scaffold(
-        appBar: AppBar(title: Text("Swipe Page"),
-        ),
-        body: Center(child: Column(children: [Text("Swipe PAge")],
-        ),
-        ),
-        ),
-    );
-    }
 
+@override
+void initState() {
+  load();
+  super.initState();
 }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Swipe Page"),
+      ),
+      body: SwipePhotos()
+    );
+  }
+}
+
+List<SwipeItem> _swipeItems = List<SwipeItem>();
+MatchEngine _matchEngine;
+GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+List<String> _names = ["Red", "Blue", "Green", "Yellow", "Orange"];
+List<Color> _colors = [
+  Colors.red,
+  Colors.blue,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange
+];
+
+class Photos {
+  Color color = null;
+  String text = null;
+  Photos(this.color, this.text);
+}
+
+void load() {
+  for (int i = 0; i < _names.length; i++) {
+    _swipeItems.add(SwipeItem(
+        content: Photos(_colors[i], _names[i]),
+        likeAction: () {
+          _scaffoldKey.currentState.showSnackBar(SnackBar(
+            content: Text("Liked ${_names[i]}"),
+            duration: Duration(milliseconds: 500),
+          ));
+        },
+        nopeAction: () {
+          _scaffoldKey.currentState.showSnackBar(SnackBar(
+            content: Text("Nope ${_names[i]}"),
+            duration: Duration(milliseconds: 500),
+          ));
+        },
+        superlikeAction: () {
+          _scaffoldKey.currentState.showSnackBar(SnackBar(
+            content: Text("Superliked ${_names[i]}"),
+            duration: Duration(milliseconds: 500),
+          ));
+        }));
+  }
+
+  _matchEngine = MatchEngine(swipeItems: _swipeItems);
+}
+
+class SwipePhotos extends StatefulWidget {  
+  
+  @override
+  SwipePhotoState createState() {
+    
+    return SwipePhotoState();
+  }
+}
+
+class SwipePhotoState extends State<SwipePhotos> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+            child: Column(children: [
+          Container(
+            height: 550,
+            child: SwipeCards(
+              matchEngine: _matchEngine,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: _swipeItems[index].content.color,
+                  //width: double.infinity,
+                  child: Text(
+                    _swipeItems[index].content.text,
+                    style: TextStyle(fontSize: 100),
+                  ),
+                );
+                  },
+                  onStackFinished: () {
+                    _scaffoldKey.currentState.showSnackBar(SnackBar(
+                      content: Text("Stack Finished"),
+                      duration: Duration(milliseconds: 500),
+                    ));
+                  },
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        _matchEngine.currentItem.nope();
+                      },
+                      child: Text("Nope")),
+                  ElevatedButton(
+                      onPressed: () {
+                        _matchEngine.currentItem.superLike();
+                      },
+                      child: Text("Superlike")),
+                  ElevatedButton(
+                      onPressed: () {
+                        _matchEngine.currentItem.like();
+                      },
+                      child: Text("Like"))
+                ],
+              )
+            ]));
+  }
+}
