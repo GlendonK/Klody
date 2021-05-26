77a06bec1fd74250283bba0f4aa9a7bd391cb042
GlendonK <36408879+GlendonK@users.noreply.github.com>
2021-05-27 02:21:43 +0800

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
         title: "Khlody",
         theme: ThemeData(
           primaryColor: Color(KhlodyTheme.primaryTheme),
